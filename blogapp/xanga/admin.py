b6cbd2c3b13c038e339934f9a3283adae142ff82
Spencer Tse <41532715+spencertse122@users.noreply.github.com>
2025-06-25 18:28:21 -0500

    Fix typos in settings and admin

--- blogapp/xanga/admin.py
+++ blogapp/xanga/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
-    prepopuilated_fields = {'slug': ('title',)}    
+    prepopulated_fields = {'slug': ('title',)}
 
 
 
